caa6399172f90feb7421b5c18d17e87900c72300
joshuadevp <joshuadevp@gmail.com>
2022-04-12 10:11:01 -0700

    Outline of files and food truck item

---
+++ app/src/main/java/com/example/project1/FoodTruck.kt
@@ -0,0 +1,11 @@
+package com.example.project1
+
+data class FoodTruck(
+    var ID: Int,
+    var name: String,
+    var imgNum: Int,
+    var loc: String,
+    var time: String,
+    var desc: String,
+    var link: String
+)
