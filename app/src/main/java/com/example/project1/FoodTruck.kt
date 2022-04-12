98393619e8ae1aae87b3a0b3b88692973471487b
hodangkhoinguyen <50374959+hodangkhoinguyen@users.noreply.github.com>
2022-04-12 09:36:20 -0700

    Phase 0 and 1 have been done

---
+++ app/src/main/java/com/example/project1/FoodTruck.kt
@@ -0,0 +1,11 @@
+package com.example.project1
+
+data class FoodTruck(
+    var ID: Int,
+    var name: String,
+    var Image: Int,
+    var location: String,
+    var open: String,
+    var description: String,
+    var link: String
+)
